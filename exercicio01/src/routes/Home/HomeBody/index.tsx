ef95f2a7a71cc4e0f551448b37b887645f4799a8
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-17 10:31:17 -0300

    Basic route structure

---
+++ exercicio01/src/routes/Home/HomeBody/index.tsx
@@ -0,0 +1,7 @@
+export default function HomeBody(){
+    return(
+        <main>
+            <h2>Home body</h2>
+        </main>
+    );
+}
---
+++ exercicio01/src/routes/Home/Promotion/index.tsx
@@ -0,0 +1,7 @@
+export default function Promotion(){
+    return(
+        <main>
+            <h2>Promotion</h2>
+        </main>
+    );
+}
