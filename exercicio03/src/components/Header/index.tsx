96ee0cf32e69f53515c83ff1ff668843589de7ca
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-17 14:12:02 -0300

    Exercicio 03

---
+++ exercicio03/src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+
+    return (
+        <header>
+            <div className="header-content-container container">
+                    <Link to="/">
+                        <h1 className="site-name">MyRoutes</h1>     
+                    </Link>
+                            
+                    <nav className="navbar">
+                        <NavLink to="/home" className={({isActive}) => isActive ? "menu-item menu-active" : "menu-item" } >
+                            Início
+                        </NavLink>
+                        <NavLink to="/promotion" className={({isActive}) => isActive ? "menu-item menu-active" : "menu-item" } >
+                            Promoção
+                        </NavLink>
+                        <NavLink to="/sub" className={({isActive}) => isActive ? "menu-item menu-active" : "menu-item" } >
+                            Participar
+                        </NavLink>
+                    </nav>  
+            </div>
+        </header>
+    );
+}
